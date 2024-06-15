11bf7716b9db3bf5fdd2b8a57fd73b82561f087e
vuanhtrankcl <k23129286@kcl.ac.uk>
2024-06-15 21:59:45 +0100

    update

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import User from '../models/user';
+import User from '../models/users';
 import { MongoServerError } from 'mongodb';
 import Account from '../models/account';
 import env from '../config/env';
--- src/models/index.ts
+++ src/models/index.ts
@@ -1,7 +1,7 @@
 import Dialog from './dialog';
 import Account from './account';
 import Scenario from './scenario';
-import User from './user';
+import User from './users';
 import Message from './message';
 
 export { Dialog, Account, Scenario, User, Message };
