36abc88cf89cf3823a36a16408a6913614d3a854
vuanhtrankcl <k23129286@kcl.ac.uk>
2024-06-24 12:27:57 +0100

    up

--- src/config/env.ts
+++ src/config/env.ts
@@ -4,7 +4,7 @@ dotenv.config();
 
 const env = {
   mongoUri: process.env.ATLAS_URI || '',
-  port: process.env.PORT || 8080,
+  port: process.env.PORT || 9090,
   jwtSecret: process.env.JWT_SECRET || '',
   llamaApi: process.env.LLAMA_URL || '',
 };
